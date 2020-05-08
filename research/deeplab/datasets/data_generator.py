236a33c6d5771b22cd4600f867854b941b838147
steven <steven.bucaille.etu@univ-lille.fr>
2020-05-08 21:12:10 +0200

    Adapt for Pascal 2009

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -99,10 +99,21 @@ _ADE20K_INFORMATION = DatasetDescriptor(
     ignore_label=0,
 )
 
+_PASCAL_VOC2009_SEG_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train' : 749,
+        'trainval' : 1499,
+        'val' : 750
+    },
+    num_classes=21,
+    ignore_label=255
+)
+
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'pascal_2009': _PASCAL_VOC2009_SEG_INFORMATION
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
